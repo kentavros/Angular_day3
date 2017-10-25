d80068ac9dae6478733a354e2f9c73c31dcdb190
student <student@GFL-1465.ad.geeksforless.net>
2017-10-25 20:36:08 +0300

    1

--- app/people.js
+++ app/people.js
@@ -1,7 +1,7 @@
 angular.module('config2').component('people', {
   bindings: { people: '<' },
   
-  template: '<h3>some individuals:</h3>' +
+  template: '<h3 style="color: darkblue">some individuals:</h3>' +
             '<ul>' +
             '  <li ng-repeat="person in $ctrl.people" style="list-style-type: none">' +
             '    <a class="link" ui-sref="person({ personId: person.id })">' +
--- app/person.js
+++ app/person.js
@@ -1,6 +1,6 @@
 angular.module('config2').component('person', {
   bindings: { person: '<' },
-  template: '<h3>A person!</h3>' +
+  template: '<h3>Tacke it:</h3>' +
   
             '<div><strong>Name: {{$ctrl.person.name}}</strong></div>' +
             '<div style="">Id: {{$ctrl.person.id}}</div>' +
