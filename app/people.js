319c26d5ab2da634323452b68209ee54bafb6224
student <student@GFL-1465.ad.geeksforless.net>
2017-10-25 20:34:15 +0300

    1

--- app/people.js
+++ app/people.js
@@ -1,10 +1,10 @@
 angular.module('config2').component('people', {
   bindings: { people: '<' },
   
-  template: '<h3>Some Peopls:</h3>' +
+  template: '<h3>some individuals:</h3>' +
             '<ul>' +
-            '  <li ng-repeat="person in $ctrl.people">' +
-            '    <a ui-sref="person({ personId: person.id })">' +
+            '  <li ng-repeat="person in $ctrl.people" style="list-style-type: none">' +
+            '    <a class="link" ui-sref="person({ personId: person.id })">' +
             '      <strong>{{person.name}}</strong>' +
             '    </a>' +
             '  </li>' +
--- app/person.js
+++ app/person.js
@@ -3,10 +3,6 @@ angular.module('config2').component('person', {
   template: '<h3>A person!</h3>' +
   
             '<div><strong>Name: {{$ctrl.person.name}}</strong></div>' +
-            
-            '<div>Company: {{$ctrl.person.company}}</div>' +
-            '<div>Email: {{$ctrl.person.email}}</div>' +
-            '<div>Address: {{$ctrl.person.address}}</div>' +
-            
-            '<button ui-sref="people">Close</button>'
+            '<div style="">Id: {{$ctrl.person.id}}</div>' +
+            '<button class="btn alert-danger" ui-sref="people">Back</button>'
 });
